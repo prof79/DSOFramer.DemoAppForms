1c37241fc0b8db6c5ad96a107a3b4ac5d9d25ce8
prof79 <markus@markusegger.at>
2025-02-16 16:11:59 +0100

    Minor refactoring.

--- src/DSOFramer.DemoAppForms/MainForm.cs
+++ src/DSOFramer.DemoAppForms/MainForm.cs
@@ -14,11 +14,14 @@ namespace DSOFramer.DemoAppForms
         }
         
         private AxFramerControl _axFramerControl;
+
         private void MyInitializeComponent()
         {
             //this.axFramerControl1.Dock = DockStyle.Fill;
             _axFramerControl = new AxFramerControl { Dock = DockStyle.Fill };
+
             this.splitContainer1.Panel1.Controls.Add(_axFramerControl);
+
             this.splitContainer1.FixedPanel = FixedPanel.Panel2;
         }
 
@@ -29,10 +32,12 @@ namespace DSOFramer.DemoAppForms
         private void btnLoad_Click(object sender, System.EventArgs e)
         {
             var openFileDialog = new OpenFileDialog();
+
             openFileDialog.InitialDirectory = System.Environment.CurrentDirectory;
             openFileDialog.Filter = @"Microsoft PPT Files|*.ppt|All Files|*.*";
             openFileDialog.RestoreDirectory = true;
             openFileDialog.FilterIndex = 1;
+
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 this._axFramerControl.Open(openFileDialog.FileName);
